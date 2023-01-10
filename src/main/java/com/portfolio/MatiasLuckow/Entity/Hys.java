fc40ec2dc29ac1c3dcc688881fec57a3eabd43b7
matiasluckow <matiluckow@hotmail.com.ar>
2023-01-10 01:02:37 -0300

    Back End Luckow Matias Ian

---
+++ src/main/java/com/portfolio/MatiasLuckow/Entity/Hys.java
@@ -0,0 +1,29 @@
+package com.portfolio.MatiasLuckow.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Hys {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private int porcentaje;
+    private String imagen;
+
+    public Hys() {
+    }
+
+    public Hys(String nombre, int porcentaje, String imagen) {
+        this.nombre = nombre;
+        this.porcentaje = porcentaje;
+        this.imagen = imagen;
+    }
+}
